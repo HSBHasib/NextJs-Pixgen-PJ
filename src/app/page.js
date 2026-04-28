6ed3e339c827632f9636e48ccf1b099c79bdacae
Hasib <hasibhsb19@gmail.com>
2026-04-28 16:55:01 +0600

    add navbar section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Banner from "./components/HomePage/Banner/Banner";
 
 export default function Home() {
   return (
     <div>
-      Root Page
+      <Banner />
     </div>
   );
 }
