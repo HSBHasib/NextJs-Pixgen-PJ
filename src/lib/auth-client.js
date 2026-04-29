23cc203e154031c36e6cd12aff184fce6aeeaa7d
Hasib <hasibhsb19@gmail.com>
2026-04-29 19:00:51 +0600

    update vercel path

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,8 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    // baseURL: "https://next-js-pixgen-pj.vercel.app"
-    baseURL: "http://localhost:3000"
+    baseURL: "https://next-js-pixgen-pj.vercel.app"
 })
 
 export const { signIn, signUp, signOut, useSession } = createAuthClient()
